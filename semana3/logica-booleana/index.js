796b6ed4b3ef3b7409469f8b6fecdba96c4f2a8d
Diego <di__e__go@hotmail.com>
2020-04-27 16:56:02 -0300

    JS incompleto

---
+++ semana3/logica-booleana/index.js
@@ -0,0 +1,34 @@
+/*1.
+    a.false
+    b.false
+    c.true
+    d.false
+    e.boolean
+
+   2.
+    a. É a maneia de se guardar várias informações dentro do código. Se declara da seguinte forma: let array = [1, 2, 3]
+    b.0
+    c. Com o .lenght
+    d.  I. undefined
+        II. null
+        III. 11
+        YV. 3 e 4
+        V. 19 e 9
+        VI. 3
+        VII. 1
+
+    */
+
+  
+//1
+//a 
+let fah = 77
+let celsius= 30
+const grausFgrausK =  ((fah - 32)*5/9 + 273.15)
+const grausCgrausF =  ((celsius)*9/5 + 32)
+const grausCgrausK =  (grausCgrausF - 32)*5/9 + 273.15
+
+console.log (grausFgrausK)
+
+console.log (grausCgrausF)
+console.log (grausCgrausF , grausCgrausK)
