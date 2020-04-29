958d379ad529eaa4ed40f7357695868bf1b8b822
Diego <di__e__go@hotmail.com>
2020-04-29 16:49:48 -0300

    Atividade loops incompleta

---
+++ semana3/loops/index.js
@@ -0,0 +1,9 @@
+/* 
+1. O código tá incrementando o valor da váriavel i em +1 e somando com o valor de sum atual. Printa na tela = 105.
+2.
+a. Ele adiciona o item no array novaLista caso o resto da sua divisão seja zero.
+b. [10, 15, 25, 30]
+c. com 3: [12,15,18,21,27,30]
+   com 4: [12]
+
+*/
