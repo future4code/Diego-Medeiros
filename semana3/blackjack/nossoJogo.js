7c7afa2536fc409ea2ee981a1d159255ca794f71
Diego <di__e__go@hotmail.com>
2020-05-03 23:30:56 -0300

    Nosso jogo Completo

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -10,3 +10,72 @@
  * 
  * 
  */
+
+console.log ("Bem vindo ao jogo Blackjack!")
+if(confirm("Deseja realizar uma rodada?")) {
+let pontuacaoUsuario = 0
+const primeiraCartaUsuario = comprarCarta()
+pontuacaoUsuario += primeiraCartaUsuario.valor
+
+
+
+let pontuacaoComputador = 0
+const primeiraCartaComputador = comprarCarta()
+pontuacaoComputador += primeiraCartaComputador.valor
+
+
+
+let palavrasMensagemComputador = ["Computador - cartas:", primeiraCartaComputador.texto, "pontuação:", pontuacaoComputador]
+let palavrasMensagemUsuario = ["Usuário - cartas:", primeiraCartaUsuario.texto, "pontuação:", pontuacaoUsuario]
+let mensagem = ""
+let mensagem2 = ""
+
+for (let trecho of palavrasMensagemComputador) {
+   mensagem = mensagem + trecho + " "
+}
+
+for (let trecho2 of palavrasMensagemUsuario) {
+   mensagem2 = mensagem2 + trecho2 + " "
+}
+
+console.log (mensagem)
+console.log (mensagem2)
+
+
+   // o que fazer se o usuário clicar "ok"
+   if (pontuacaoUsuario > pontuacaoComputador) {
+      
+      console.log ("Usuário ganhou")
+   }
+
+   else if (pontuacaoUsuario < pontuacaoComputador) {
+   
+   console.log ("Computador ganhou")
+}
+
+   else if (pontuacaoUsuario = pontuacaoComputador) {
+
+   console.log ("Empate!")
+}
+}
+
+else {
+   // o que fazer se o usuário clicar "cancelar"
+   console.log ("O jogo acabou!")
+} 
+
+
+
+
+
+/* Exemplo de deixar tudo como string
+let palavras = ["Oi", "sumida", "tudo", "bem?"]
+let mensagem = ""
+
+for (let palavra of palavras) {
+    mensagem = mensagem + palavra + " "
+}
+
+console.log(mensagem)
+*/
+
