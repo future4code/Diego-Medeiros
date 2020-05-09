cc2c5ca577548f145df6bc1157a2b83fadbaef36
Diego <di__e__go@hotmail.com>
2020-05-09 19:32:25 -0300

    Lógica de Programação 1

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,14 @@
+/*
+1. Podemos percorrer valores de uma lista, especificamente falando, de um array, podemos usar métodos de laço que é o for, for of e for each.  
+
+Nesse exemplo percorro todo o array "numeros" para achar os numeros menores de 7. 
+let numeros= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numeroMenor=[0]
+for (numero of numeros) {
+  if (numero < 7) {
+    numeroMenor.push(numero)   
+  }
+}
+console.log(numeroMenor)
+
+*/
