7c20e67bbedcc233e305ad5dc1bacdda67eb45ac
Diego <di__e__go@hotmail.com>
2020-05-12 16:46:58 -0300

    Labedin

---
+++ semana5/labetube/build/precache-manifest.172993583ab7dcd9f3bb87cfde6c4293.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e2edf33e35157db3129e62bbc757ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ba3691590213fa8ebe8",
+    "url": "/static/css/main.deb9aeaf.chunk.css"
+  },
+  {
+    "revision": "9f4ddf894b7e37a854ba",
+    "url": "/static/js/2.2abe7b8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2abe7b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba3691590213fa8ebe8",
+    "url": "/static/js/main.17b3d329.chunk.js"
+  },
+  {
+    "revision": "5f2fd202c8dcfabf0c4f",
+    "url": "/static/js/runtime-main.1f002b85.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
