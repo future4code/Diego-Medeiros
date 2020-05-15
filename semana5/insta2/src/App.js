e2a31274c16197c3b8e46d94f5565e51fc9b8b02
Diego <di__e__go@hotmail.com>
2020-05-15 03:03:25 -0300

    Insta4_2.atividade com array

--- semana5/insta2/src/App.js
+++ semana5/insta2/src/App.js
@@ -1,29 +1,47 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
+import { render } from '@testing-library/react';
+import foto1 from './1.png'
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'joao'}
-          fotoUsuario={'https://picsum.photos/50/51'}
-          fotoPost={'https://picsum.photos/200/151'}
-        />
-        <Post
-          nomeUsuario={'amanda'}
-          fotoUsuario={'https://picsum.photos/50/52'}
-          fotoPost={'https://picsum.photos/200/152'}
-        />
-      </div>
-    );
+state = {
+  
+  posts: [
+    {
+      nomeUsuario:"paulinha",
+      fotoUsuario:foto1,
+      fotoPost:"https://picsum.photos/200/150"
+    },
+    {
+      nomeUsuario:"joao",
+      fotoUsuario:"https://picsum.photos/50/51",
+      fotoPost:"https://picsum.photos/200/151"
+    },
+    {
+      nomeUsuario:"amanda",
+      fotoUsuario:"https://picsum.photos/50/52",
+      fotoPost:"https://picsum.photos/200/152"
+    }
+  ]
+};
+
+render() {
+
+  const listadePosts = this.state.posts.map(postagem =>  {
+
+  return (
+    <div>
+    <div>{postagem.nomeUsuario}</div>
+    <div>{postagem.fotoUsuario}</div>
+    <div>{postagem.fotoPost}</div>
+    </div>
+  );
+  });
+
+  return ( 
+    <div>{listadePosts}</div>
+  );
   }
 }
-
-export default App;
+export default App
