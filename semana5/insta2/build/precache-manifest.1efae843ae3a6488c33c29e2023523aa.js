5dcce3fba937104c55447a7a3418793cb2a3cbb4
Diego <di__e__go@hotmail.com>
2020-05-18 17:02:13 -0300

    Criação da pasta para atividade

---
+++ semana5/insta2/build/precache-manifest.1efae843ae3a6488c33c29e2023523aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4fda7b10bcb836ab775d37f63ded1c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "759d950b66ae034b747a",
+    "url": "/static/css/main.a5db72f3.chunk.css"
+  },
+  {
+    "revision": "280742c03ef81115be1e",
+    "url": "/static/js/2.4d7f8360.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.4d7f8360.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759d950b66ae034b747a",
+    "url": "/static/js/main.9053c8ce.chunk.js"
+  },
+  {
+    "revision": "257570d7eb86fa8a07ed",
+    "url": "/static/js/runtime-main.b6564e12.js"
+  },
+  {
+    "revision": "d6e56a31ae91a759062811d72e731f74",
+    "url": "/static/media/1.d6e56a31.png"
+  },
+  {
+    "revision": "de299b47c15bad3a33ad36319a10bef3",
+    "url": "/static/media/comment_icon.de299b47.svg"
+  },
+  {
+    "revision": "042aa417f110588538aefef728ed90fa",
+    "url": "/static/media/favorite-white.042aa417.svg"
+  },
+  {
+    "revision": "4c67d8751e56b25704ac9b791835bef8",
+    "url": "/static/media/favorite.4c67d875.svg"
+  }
+]);
