177a949faa6643e60b7b6ed268b199cae50e0634
Diego <di__e__go@hotmail.com>
2020-05-19 16:48:29 -0300

    Inicio da Atividade, ListaDeTarefas

---
+++ semana6/labenuforms/build/precache-manifest.9163f499e2af1b8c51381415b01a1d29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f7852c397114e75c7ad25c5557d116",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f11f2f6dadeb77dd6151",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b4481e946e4097077b80",
+    "url": "/static/js/2.3cf333ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3cf333ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f11f2f6dadeb77dd6151",
+    "url": "/static/js/main.e6eea3af.chunk.js"
+  },
+  {
+    "revision": "f1e08efe6fdab0b21df5",
+    "url": "/static/js/runtime-main.a3533f7c.js"
+  }
+]);
