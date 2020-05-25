cd76b4ec4dfdc1f33a00277722074fa95d1538c7
Diego <di__e__go@hotmail.com>
2020-05-24 22:43:52 -0300

    Deploy

---
+++ semana6/lista-tarefas/build/precache-manifest.d9d5c5cd8ea7fae1260b05dd30b64390.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1a56faf843666add6ab0e5bf057eb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73fb214c7347755214a0",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "28e1f6e4c00be6f90c95",
+    "url": "/static/js/2.8fdc7c9c.chunk.js"
+  },
+  {
+    "revision": "73fb214c7347755214a0",
+    "url": "/static/js/main.ac4cf435.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
