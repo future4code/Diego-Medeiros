b9db9b4a97d50c21c9f2ddd5f13cec22c17f466b
Diego <di__e__go@hotmail.com>
2020-08-04 17:06:37 -0300

    Começo da atividade

---
+++ semana12/aula49-static-heranca/src/index.ts
@@ -0,0 +1,55 @@
+class User {
+  private id: string;
+  private email: string;
+  private name: string;
+  private password: string;
+
+  constructor(id: string, email: string, name: string, password: string) {
+    console.log("Chamando o construtor da classe User");
+    this.id = id;
+    this.email = email;
+    this.name = name;
+    this.password = password;
+  }
+
+  public getId(): string {
+    return this.id;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+}
+
+const usuarios: User = new User("1", "saruman@saruman.com", "Saruman", "1234");
+console.log(usuarios.getId(), usuarios.getName(), usuarios.getEmail());
+
+//A- Não, precisaria criar um public get, pois password está private
+//B- 1 vez
+
+class Customer extends User {
+  public purchaseTotal: number = 0;
+  private creditCard: string;
+
+  constructor(
+    id: string,
+    email: string,
+    name: string,
+    password: string,
+    creditCard: string
+  ) {
+    super(id, email, name, password);
+    console.log("Chamando o construtor da classe Customer");
+    this.creditCard = creditCard;
+  }
+
+  public getCreditCard(): string {
+    return this.creditCard;
+  }
+}
+
+const cliente: Customer = new Customer();
